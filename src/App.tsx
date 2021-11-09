98181c333dc9eb55f85ec9016aebb08e8d30294a
ilyaUsoltsev <ilia.a.usoltsev@gmail.com>
2021-11-09 19:04:35 +0300

    randomizing cards and displaying top cover

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import './App.css';
+import * as Styled from './app.styles';
+
+const pictures = [
+  'cat',
+  'dog',
+  'mood',
+  'moon',
+  'phone',
+  'sand',
+  'sky',
+  'soccer',
+];
+
+const preparedCards = [...pictures, ...pictures]
+  .map((item) => ({ word: item, id: Math.random() }))
+  .sort((a, b) => a.id - b.id);
 
 function App() {
-  return <div>Card game</div>;
+  return (
+    <Styled.Container className='container'>
+      {preparedCards.map((card) => (
+        <Styled.Box className='box' key={card.id}>
+          <Styled.Cover />
+        </Styled.Box>
+      ))}
+    </Styled.Container>
+  );
 }
 
 export default App;
---
+++ src/app.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import cover_image from './pictures/card-top.jpeg';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  margin: 32px auto;
+  max-width: 800px;
+  row-gap: 30px;
+`;
+
+export const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  position: relative;
+`;
+
+export const Cover = styled.div`
+  background-image: url(${cover_image});
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+`;
